961eaa82c11607cc8fc407c852c4a8e5c91c6aea
Spencer Taira <spencermtaira@gmail.com>
2022-10-24 12:39:31 -0700

    added view functions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Cupcake
@@ -20,3 +20,45 @@ app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/api/cupcakes')
+def cupcakes_info():
+    """ Return JSON with cupcakes info """
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def cupcake_info(cupcake_id):
+    """ Return JSON with cupcake info """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post('/api/cupcakes')
+def cupcake_add():
+
+
+    flavor = request.json['flavor']
+    size = request.json['size']
+    rating = request.json['rating']
+    image = request.json['image']
+
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image=image
+    )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+    return (jsonify(cupcake=serialized), 201)
+
+
