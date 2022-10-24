95108f27e23300bee51c55c862c38c2ad76f426e
trevorhudson <trevor.c.hudson@gmail.com>
2022-10-24 12:03:12 -0700

    created cupcake class, ininitialize database

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 """Flask app for Cupcakes"""
+from flask import Flask, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,32 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    """Individual Cupcake"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String(20),
+                       nullable=False)
+    size = db.Column(db.String(10),
+                     nullable=False)
+    rating = db.Column(db.Integer,
+                       nullable=False)
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default='https://tinyurl.com/demo-cupcake')
+
+
+# DO NOT MODIFY THIS FUNCTION
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
