60c36c51c08be0d013f946167ddbb4550caa35be
trevorhudson <trevor.c.hudson@gmail.com>
2022-10-24 17:03:41 -0700

    added eventHandler to submit new cupcake to page

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const BASE_API_URL = "localhost:5000/api"
-function main() {
+const BASE_API_URL = "http://localhost:5001/api";
+const $cupcakesContainer = $('#cupcakes-container');
+const $addCupcakeForm = $('#cupcake-add-form');
 
-}
 
 /**
  * Makes axios get request to BASE_API_URL
@@ -12,38 +12,64 @@ function main() {
  */
 
 async function getCupcakes() {
-  response = await axios.get(BASE_API_URL);
+  const response = await axios.get(BASE_API_URL + '/cupcakes');
   return response.data.cupcakes;
 }
 
-/*
-<ol>
-for
-  <li></li>
-</ol>
-*/
-async function generateCupcakeHTML(cupcake) {
+/**
+ * Creates jQuery Object for a single cupcake
+ * Input: cupcake object
+ * Output: jQuery object of cupcake with HTML.
+ */
+function generateCupcake(cupcake) {
   return $("<div>")
     .attr('id', `${cupcake.id}`)
-    .append($('<img>'))
-    .append(`<p>${cupcake.flavor} Cupcake,
+    .addClass('cupcake')
+    .append($(`<img src = "${cupcake.image}">`))
+    .append(`<p>Flavor: ${cupcake.flavor},
     Size: ${cupcake.size},
-    Rating: ${cupcake.rating}</p>`)
+    Rating: ${cupcake.rating}</p>`);
 }
 
-// <image>
-// <flavor> Cupcake
-    // - size, rating
+/**
+ * Generate jQuery objects for list of cupcakes, and appends HTML to the DOM.
+ * Inputs: None
+ * Output: None
+ */
 
+async function showCupcakes() {
+  const cupcakes = await getCupcakes();
 
-/*
-  <div class="row"
-    <div class="col">
-      <img>
-      <p> <Flavor>
-    </div>
-    <div class="col">
-      <p>
-    </div>
-  </div>
-*/
+  for (let cupcake of cupcakes) {
+    $cupcakesContainer.append(generateCupcake(cupcake));
+  }
+
+}
+
+/**
+ * Handle form submission to add a new cupcake to the database.
+ * Refreshes page with new cupcake.
+ * Inputs: None
+ * Outputs: None
+ */
+async function addCupcake(evt) {
+
+  const flavor = $('#flavor').val();
+  const rating = $('#rating').val();
+  const size = $('#size').val();
+  const image = $('#image').val();
+
+  const response = await axios.post(
+    BASE_API_URL + '/cupcakes',
+    {
+      flavor,
+      rating,
+      size,
+      image
+    }
+  );
+}
+
+showCupcakes();
+
+$addCupcakeForm.on('submit', addCupcake);
